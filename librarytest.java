232e250d52d030b9396b4c14d6fea03c56978102
Sayuj Pillai <Sayujpillai63@gmail.com>
2025-02-26 17:07:49 +0530

    Add files via upload

---
+++ librarytest.java
@@ -0,0 +1,7 @@
+import librarypackage.Book;
+public class librarytest{
+    public static void main(String[] args) {
+        Book book1 = new Book("xyz", "abc", "978273565");
+        book1.displayInfo();
+    }
+}
