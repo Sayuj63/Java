37c9f0b7267ac1a6bcc8292f43ac7e75c5c9ddfe
Sayuj Pillai <Sayujpillai63@gmail.com>
2025-03-02 22:27:11 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,16 @@
+public class methods 
+{
+    public void Display()
+    {
+        int a=10;
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        System.err.println("try programiz.pro");
+        methods obj = new methods();
+        obj.Display();
+    }
+}
+   
+    
+
